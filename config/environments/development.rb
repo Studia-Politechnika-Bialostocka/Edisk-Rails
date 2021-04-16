26c6f3d39de7c231b6a94cd30e46867af054a7f5
mateuszbialowas <mateusz.bialowas2@gmail.com>
2021-04-16 22:01:49 +0200

    added gmail configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -81,12 +81,11 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name:      ENV['SENDMAIL_USERNAME'],
-    password:       ENV['SENDMAIL_PASSWORD'],
-    domain:         ENV['MAIL_HOST'],
+    user_name:      Rails.application.credentials.sendmail_username,
+    password:       Rails.application.credentials.sendmail_password,
+    domain:         "localhost:3000",
     address:       'smtp.gmail.com',
     port:          '587',
     authentication: :plain,
--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,15 @@ Rails.application.routes.draw do
 
   as :user do
     # sessions
-    get    'login',  to: 'devise/sessions#new',     as: :new_user_session
-    post   'login',  to: 'devise/sessions#create',  as: :user_session
-    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+    get    'login',  to: 'devise/sessions#new',           as: :new_user_session
+    post   'login',  to: 'devise/sessions#create',        as: :user_session
+    delete 'logout', to: 'devise/sessions#destroy',       as: :destroy_user_session
 
     # registrations
-    get 'cancel', to: 'devise/registrations#cancel', as: :cancel_user_registration
-    get 'sign_up', to: 'devise/registrations#new', as: :new_user_registration
-    get 'edit', to: 'devise/registrations#edit', as: :edit_user_registration
-    patch 'users', to: 'devise/registrations#update', as: :user_registration
+    get 'cancel', to: 'devise/registrations#cancel',      as: :cancel_user_registration
+    get 'sign_up', to: 'devise/registrations#new',        as: :new_user_registration
+    get 'edit', to: 'devise/registrations#edit',          as: :edit_user_registration
+    patch 'users', to: 'devise/registrations#update',     as: :user_registration
     put 'users', to: 'devise/registrations#update'
     delete 'users', to: 'devise/registrations#destroy'
     post 'users', to: 'devise/registrations#create'
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
     # passwords
     get   'password/new',  to: 'devise/passwords#new',    as: :new_user_password
     get   'password/edit', to: 'devise/passwords#edit',   as: :edit_user_password
-    patch 'password', to: 'devise/passwords#update', as: :user_password
+    patch 'password', to: 'devise/passwords#update',      as: :user_password
     post  'password',  to: 'devise/passwords#create'
     put   'password', to: 'devise/passwords#update'
   end
