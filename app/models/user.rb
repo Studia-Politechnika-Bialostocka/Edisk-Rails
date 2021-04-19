e4e9f9b66a3774453429d706c651211cb6469ed3
mateuszbialowas <mateusz.bialowas2@gmail.com>
2021-04-19 23:03:08 +0200

    fixed bugs in views

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
-  validates :password, length: { minimum: 8 }, format: { with: /[A-Za-z0-9][^A-Za-z0-9\s]|[^A-Za-z0-9\s][A-Za-z0-9]/,
+  validates :password, format: { with: /[A-Za-z0-9][^A-Za-z0-9\s]|[^A-Za-z0-9\s][A-Za-z0-9]/,
                                                         message: "at least one special character and a letter/number" }
 
   validates :username, uniqueness: true, length: { minimum: 2}
