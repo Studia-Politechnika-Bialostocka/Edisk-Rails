4edb5b3a0da2224fcb649d18dcb58ee3ffb38ec3
mateuszbialowas <mateusz.bialowas2@gmail.com>
2021-04-22 13:38:59 +0200

    add favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,8 +11,8 @@
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
+
+const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
 console.log('Hello World from Webpacker')
@@ -21,3 +21,5 @@ import '../styles/site.scss'
 import '../styles/navbarmain.scss'
 import '../styles/navbar.css'
 import '../styles/home-page.scss'
+// import '../images/brand_icon.ico'
+// const images = require.context('.../images', true)
