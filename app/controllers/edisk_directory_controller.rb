ecc41284066f10901fd306699a8d52a0eb911e74
me3eh <matt30002000@gmail.com>
2021-04-30 17:02:07 +0200

    Did a little refactoring. Without tests and error checks

--- app/controllers/edisk_directory_controller.rb
+++ app/controllers/edisk_directory_controller.rb
@@ -2,36 +2,33 @@ class EdiskDirectoryController < ApplicationController
   before_action :authenticate_user!
 
   def new
-    @edisk_directory = EdiskDirectory.new
+    @edisk_directory = current_user.edisk_directories.children_of(params[:format]).new
   end
 
   def create
-
-    @edisk_directory = current_user.edisk_directories.children_of(params[:id]).new(edisk_directory_params)
-
+    @edisk_directory = current_user.edisk_directories.children_of(params[:parent_id]).new(edisk_directory_params)
+    temp1 = EdiskDirectory.find(@edisk_directory.parent_id)
     if @edisk_directory.save
-      redirect_to "/edisk_directories/" + @edisk_directory.parent_id.to_s, notice: "Succesfully created"
+      redirect_to edisk_directory_path(temp1),  notice: "Succesfully created"
     else
-      render new, notice:"Something goes wrong"
+      redirect_to root_path, notice:"Something goes wrong"
     end
   end
 
   def edit
+
     @edisk_directory = EdiskDirectory.find(params[:id])
   end
   def update
       @edisk_directory = EdiskDirectory.find(params[:id])
+      temp1 = EdiskDirectory.find(@edisk_directory.parent_id)
 
       if @edisk_directory.update(edisk_directory_params)
-        redirect_to root_path
+        redirect_to edisk_directory_path(temp1), notice: "Succesfully created"
       else
         render :edit, notice: "Something goes wrong"
   end
   end
-  # def index
-  #   @edisk_directory = EdiskDirectory.all
-  # end
-
   def destroy
     @edisk_directory = EdiskDirectory.find(params[:id])
     s = @edisk_directory.parent_id
@@ -42,7 +39,6 @@ class EdiskDirectoryController < ApplicationController
 
   def show
     @edisk_directory = EdiskDirectory.find(params[:id])
-    @edisk_directories = nil
     @edisk_directories = EdiskDirectory.children_of(@edisk_directory).where(user_id: current_user.id)
     if @edisk_directories == nil
       "Nothing to show"
