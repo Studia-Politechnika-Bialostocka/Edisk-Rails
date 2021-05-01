e31190b572937610dc41a473dd57823a9fe510e2
me3eh <matt30002000@gmail.com>
2021-05-01 14:38:47 +0200

    Added errors, no tests

--- app/controllers/edisk_directory_controller.rb
+++ app/controllers/edisk_directory_controller.rb
@@ -1,5 +1,10 @@
 class EdiskDirectoryController < ApplicationController
   before_action :authenticate_user!
+  rescue_from ::ActiveRecord::RecordNotFound, with: :record_not_found
+
+  def record_not_found(exception)
+    redirect_to root_path, alert: "Nie istnieje taka strona"
+  end
 
   def new
     @edisk_directory = current_user.edisk_directories.children_of(params[:format]).new
@@ -11,7 +16,7 @@ class EdiskDirectoryController < ApplicationController
     if @edisk_directory.save
       redirect_to edisk_directory_path(temp1),  notice: "Succesfully created"
     else
-      redirect_to root_path, notice:"Something goes wrong"
+      redirect_to new_edisk_directory_path(params[:parent_id]), alert: "Name can't be blank"
     end
   end
 
@@ -26,27 +31,26 @@ class EdiskDirectoryController < ApplicationController
       if @edisk_directory.update(edisk_directory_params)
         redirect_to edisk_directory_path(temp1), notice: "Succesfully created"
       else
-        render :edit, notice: "Something goes wrong"
+        redirect_to edit_edisk_directory_path(@edisk_directory), alert: "Name can't be blank"
   end
   end
   def destroy
     @edisk_directory = EdiskDirectory.find(params[:id])
-    s = @edisk_directory.parent_id
+    temp1 = @edisk_directory.parent_id
     @edisk_directory.destroy
 
-    redirect_to "/edisk_directories/" + s.to_s
+    redirect_to edisk_directory_path(temp1)
   end
 
   def show
     @edisk_directory = EdiskDirectory.find(params[:id])
     @edisk_directories = EdiskDirectory.children_of(@edisk_directory).where(user_id: current_user.id)
-    if @edisk_directories == nil
-      "Nothing to show"
-    end
   end
 
-    private
-    def edisk_directory_params
-      params.permit(:name, :path, :parent_id, :ancestry, :user_id)
-    end
+  private
+  def edisk_directory_params
+    params.permit(:name, :path, :parent_id, :ancestry, :user_id)
+  end
+
+
   end
