ef3afb4b42aed99f424d63d0afea8a1d8742d2d7
mateuszbialowas <mateusz.bialowas2@gmail.com>
2021-05-08 18:11:33 +0200

    added dependent: :destroy and first test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :edisk_directories
+  has_many :edisk_directories, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
   validates :password, format: { with: /[A-Za-z0-9][^A-Za-z0-9\s]|[^A-Za-z0-9\s][A-Za-z0-9]/,
                                                         message: "at least one special character and a letter/number" }
 
-  validates :username, uniqueness: true, length: { minimum: 2}
+  validates :username, presence: true, uniqueness: true, length: { minimum: 2}
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,8 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should now save user without username" do
+    user = User.new
+    assert_not user.save
+  end
 end
