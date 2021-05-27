b54122756efa3d3f7be9f0ea2a7638233f637450
MatiSekDev <mateuszss19@interia.pl>
2021-05-27 15:49:11 +0200

    import "chartkick/chart.js" bo znowu go skasowalo

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,6 +20,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "chartkick/chart.js"
 
 import '../stylesheets/site.scss'
 import '../stylesheets/navbarmain.scss'
